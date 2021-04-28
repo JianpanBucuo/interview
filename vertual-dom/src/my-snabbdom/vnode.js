34675f7dd990f5bb511a45c3e10012859ea0344b
jianpanbucuo <625737550@qq.com>
2021-04-28 18:27:16 +0800

    1

--- vertual-dom/src/my-snabbdom/vnode.js
+++ vertual-dom/src/my-snabbdom/vnode.js
@@ -6,6 +6,13 @@
 //         elm
 //     }
 // }
+/**
+ * @param sel 选择器String
+ * @param data 属性集合
+ * @param children 子节点
+ * @param tex 字符串
+ * @param elm Dom节点
+ */
 export default class Vnode {
     constructor(sel,data, children = [], txt ='', elm) {
         this.sel = sel
